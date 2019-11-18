2339f92dbe2934e2dea335689e7c686f3f746370
marcindewicki <marcindewicki@outlook.com>
2019-11-18 22:16:32 +0100

    uwu
    uwu

--- lib/main.dart
+++ lib/main.dart
@@ -2,11 +2,11 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(
-    Doge()
+    App()
   );
 }
 
-class Doge extends StatelessWidget {
+class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -15,79 +15,79 @@ class Doge extends StatelessWidget {
         body: SafeArea(
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: <Widget>[
-             CircleAvatar(
-                  radius: 75.0,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              CircleAvatar(
+                radius: 75.0,
                 backgroundImage: AssetImage('zdj/angry.jpg'),
-            ),
-            SizedBox(height: 15.0,),
-            Text(
-              'Dane osobowe',
-              style: TextStyle(
-                fontFamily: 'BebasNeue',
-                fontSize: 40.0,
-                color: Colors.white,
-                letterSpacing: 2.0,
               ),
-            ),
-            Text(
-              'Przykladowy tekst XD',
-              style: TextStyle(
-                fontFamily: 'Lato',
-                fontSize: 25.0,
-                color: Colors.white,
+              SizedBox(height: 15.0,),
+              Text(
+                'Dane osobowe',
+                style: TextStyle(
+                  fontFamily: 'BebasNeue',
+                  fontSize: 40.0,
+                  color: Colors.white,
+                  letterSpacing: 2.0,
+                ),
               ),
-            ),
-            SizedBox(
-              height: 10.0,
-              width: 300.0,
-              child: Divider(
-                color: Colors.teal[100],
+              Text(
+                'Przykladowy tekst XD',
+                style: TextStyle(
+                  fontFamily: 'Lato',
+                  fontSize: 25.0,
+                  color: Colors.white,
+                ),
               ),
-            ),
-            Card(
-              margin: EdgeInsets.symmetric(horizontal: 25.0, vertical: 15.0),
-              child: Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: ListTile(
-                  leading: Icon(Icons.phone,
-                  size: 45.0,
-                  color: Colors.teal[900],
-                  ),
-                  title: Text(
-                    '+48 123 456 789',
-                    style: TextStyle(
-                      fontFamily: 'Lato',
-                      fontSize: 18.0,
-                    ),
-                  ),
-                )
+              SizedBox(
+                height: 10.0,
+                width: 300.0,
+                child: Divider(
+                  color: Colors.teal[100],
+                ),
               ),
-            ),
-            SizedBox(height: 10.0,),
-            Card(
-              margin: EdgeInsets.symmetric(vertical: 15.0, horizontal: 25.0),
-              child: Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: ListTile(
-                  leading: Icon(Icons.email,
-                  size: 45.0,
-                  color: Colors.teal[900],),
-                  title: Text(
-                    'Przykladowy@Email',
-                    style: TextStyle(
-                      fontFamily: 'Lato',
-                      fontSize: 18.0,
-                    ),
-                  ),
-                )
+              Card(
+                margin: EdgeInsets.symmetric(horizontal: 25.0, vertical: 15.0),
+                child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: ListTile(
+                      leading: Icon(Icons.phone,
+                        size: 45.0,
+                        color: Colors.teal[900],
+                      ),
+                      title: Text(
+                        '+48 123 456 789',
+                        style: TextStyle(
+                          fontFamily: 'Lato',
+                          fontSize: 18.0,
+                        ),
+                      ),
+                    )
+                ),
               ),
-            )
-          ],
+              SizedBox(height: 10.0,),
+              Card(
+                margin: EdgeInsets.symmetric(vertical: 15.0, horizontal: 25.0),
+                child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: ListTile(
+                      leading: Icon(Icons.email,
+                        size: 45.0,
+                        color: Colors.teal[900],),
+                      title: Text(
+                        'Przykladowy@Email',
+                        style: TextStyle(
+                          fontFamily: 'Lato',
+                          fontSize: 18.0,
+                        ),
+                      ),
+                    )
+                ),
+              )
+            ],
+          ),
         ),
       ),
-    ),
     );;
   }
 }
